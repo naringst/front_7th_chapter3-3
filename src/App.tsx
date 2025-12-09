09a0c3e350cbef4bd5135b4ffca381e00dbc9f4a
Nari Jeong <92130993+naringst@users.noreply.github.com>
2025-12-10 03:10:08 +0900

    refactor: Header, Footer 컴포넌트 widget로 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router } from "react-router-dom"
-import Header from "./shared/ui/Header.tsx"
-import Footer from "./shared/ui/Footer.tsx"
+import Header from "./widgets/ui/Header.tsx"
+import Footer from "./widgets/ui/Footer.tsx"
 import { PostsManagerPage } from "./pages/postManager"
 
 const App = () => {
